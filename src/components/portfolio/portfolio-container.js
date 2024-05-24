6c73f96ada4f5850cc38c97dc5d69953f814a1ff
Ander <apalaciosca@gmail.com>
2024-05-24 23:45:24 +0200

    PortfolioItems

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -4,15 +4,25 @@ import PortfolioItem from './portfolio-item';
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
-        
-        console.log("Portfolio container has rendered")
+
+        console.log("Portfolio container has rendered");
     }
+
+    portfolioItems() {
+        const data = ["Quip", "Eveentbrite", "Ministry safe", "SwingAway"];
+
+
+        return data.map(item => {
+            return <PortfolioItem />;
+        })
+    }
+
     render() {
         return (
             <div>
-                <h2>Portfolio items go here...</h2>
+                <h2>Portfolio items go here updated...</h2>
 
-                <PortfolioItem />
+                {this.portfolioItems()}
             </div>
         )
     }
