151d0402833fe3982faacefe8154bb7b2368c475
Ander <apalaciosca@gmail.com>
2024-05-25 01:16:19 +0200

    this

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -5,22 +5,27 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
-        console.log("Portfolio container has rendered");
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                {title: "Quip" }, 
+                {title: "Eveentbrite" },
+                {title: "Ministry safe" }, 
+                {title: "SwingAway" }
+            ]
+        };
     }
 
     portfolioItems() {
-        const data = ["Quip", "Eveentbrite", "Ministry safe", "SwingAway"];
-
-
-        return data.map(item => {
-            return <PortfolioItem title={item} url={"google.com"} />;
-        })
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />;
+        });
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio items go here updated...</h2>
+                <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
             </div>
